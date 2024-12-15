ac8d7a8761cc056206308c7a399e49d6406b22d9
qadirq01 <67214834+qusayqadir@users.noreply.github.com>
2024-12-14 22:17:56 -0500

    dec14

--- src/data-ingestion(extract)/grab_data.py
+++ src/data-ingestion(extract)/grab_data.py
@@ -1,10 +1,40 @@
-'''
-fetch real fl data to feed into 
-1. real-time data? ( leaderboard)
-2. historical (ergast f1 api ) -> computer aggregate statistics
+import json 
+import requests
+import os 
 
 
-clean json data file if nessecary ? 
-'''
+def historical_data_fetch():    
+    type_data = ["year","round","results"]
+    APIdata_folderpath = "../../data/initial-datafetch-api/year"
+    if not os.path.exists(APIdata_folderpath):
+        os.makedirs(APIdata_folderpath)
 
-def fetch_data(): 
+    header = {"Accept" : "application/json"}
+
+    
+
+    for year in range(1950, 2024 + 1 ,1): 
+        url = f"http://ergast.com/api/f1/{year}.json"
+        response = requests.get(url, headers=header) 
+        response_data = response.json() 
+
+     
+        file_name = f"{year}.json"
+
+        folder_path = os.path.join(APIdata_folderpath, file_name)
+         # what it would look like (../../data/initial-datafetch-api/test-json.json)
+        if os.path.exists(folder_path):
+            continue
+
+
+
+        if response.status_code == 200: 
+
+            with open(folder_path, "w") as file: 
+                json.dump(response_data, file, indent=4) 
+        
+
+
+
+historical_data_fetch()
+
